01149c0d87cb043f14b90df7542e486696b17aad
MIRIAMMILDRED <miriammildred20@gmail.com>
2023-03-16 17:03:23 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+int main(void)
+{
+    char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(STDERR_FILENO, quote, sizeof(quote) - 1);
+    return 1;
+}
